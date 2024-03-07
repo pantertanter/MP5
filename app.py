85f10772f7ec4e765bd34dfa1c4060f6c4d919ce
Alex <Alexanderstubmichelsen@gmail.com>
2024-03-07 15:43:16 +0100

    So far so good

--- app.py
+++ app.py
@@ -1,5 +1,66 @@
 import streamlit as st
-from cosim import cosimfunc
-import importlib 
+from data_loader import (load_docx, load_text_file,
+                         vect)
+import pandas as pd
+from cosimfunc import cosim
+
+
+# Load the data---------------------------------------
+
+doc = load_docx("./data/BarackHusseinObamaWiki.docx")
+
+doc_text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+
+txt = load_text_file("./data/ObamaDanskeLex.txt")
+
+st.write('We loaded the data from the two files.')
+
+st.write('Data loaded successfully.')
+
+# Input text from the user
+user_input = st.text_area("Enter your text here:", "")
+
+# --------------splitting them in to words------------
+
+text1 = doc_text.split(" ")
+text2 = txt.split(" ")
+
+st.write('We split the data sets by all the spaces in the text.')
+
+# ---------------Join the texts and and remove dublicates.----------------
+
+corpus = set(text1).union(set(text2))
+
+# Calculate the length of the corpus
+corpus_length = len(corpus)
+
+# Display the length of the corpus along with a message
+st.write(f"The corpus contains {corpus_length} words/documents.")
+
+# ---------------The document is now of type set----------------
+
+dict1 = vect(text1, corpus)
+
+dict2 = vect(text2, corpus)
+
+df = pd.DataFrame([dict1, dict2])
+
+st.write('We created a binary vector of word appearance in a sentence.')
+
+st.write(df)
+
+# --------------- convert the dataframe to a numpy array---------------
+
+ar = df.to_numpy()
+
+# --------------- calculate the cosine similarity---------------
+
+
+cosim_simularity = cosim(ar[0], ar[1])
+
+st.write(f'We calculated the cosine similarity between the two vectors.')
+st.write(f'Cosine similarity: {cosim_simularity}')
+
 
 st.write("Hello, world!")
+
---
+++ data_loader.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from docx import Document
+
+# ------------------functions to load documents----------------
+
+def load_docx(path):
+    return Document(path)
+
+# Load the DOCX document
+doc = load_docx("./data/BarackHusseinObamaWiki.docx")
+
+# Extract text from paragraphs and join them into a single string
+doc_text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+
+# Display the text in a Streamlit text area
+st.text_area("Document Content", value=doc_text, height=400)
+
+def load_text_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+    return text
+
+# ------------------function to make a dict 1 means the word accours----------------
+
+# Binary vector of word appearance in a sentence
+def vect(sent, corpus):
+    # create new dict and place zeros in it
+    mydict = dict.fromkeys(corpus, 0) 
+    
+    # code each word's appearance in the sentence with 1
+    for word in sent:
+        mydict[word] = 1
+    return mydict    
